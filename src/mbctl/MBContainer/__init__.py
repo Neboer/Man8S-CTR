af7cec4c0318610e6089309338ea695085957241
Neboer <rubinposter@gmail.com>
2026-02-18 12:45:18 +0800

    删除metadata相关的所有内容。

--- src/mbctl/MBContainer/__init__.py
+++ src/mbctl/MBContainer/__init__.py
@@ -1,6 +1,5 @@
 from .MBContainer import MBContainer
 from .MBContainerMount import MBContainerMount, MBContainerMountEntry, get_mount_point_src
 from .MBContainerPort import MBContainerPortMap
-from .MBContainerMetadata import MBContainerMetadata
 from .MBContainerStatus import MBContainerStatus
 from .MBContainerTree import MBContainerTree
